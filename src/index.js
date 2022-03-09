aa71fb20ea1c4e384448aa426920feb27e2975cc
wlsdpcks <wlsdpcks@naver.com>
2022-03-09 14:53:58 +0900

    useInput

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
 const App = () => {
-  const [item, setItem] = useState(1); //기본값이 1
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
   return (
     <div classname="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>decrement</button>
+      <h1>Hello</h1>
+      <input
+        placeholder="Name"
+        value={name.value}
+        onChange={name.onChange}
+      />{" "}
+      {/* value={name.value} onChange={name.onChange} 대신 {...name}을 사용할 수 있다. */}
     </div>
   );
 };
