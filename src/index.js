6b95aa7c5777be11cc5c37a20c887a73e103fe6c
wlsdpcks <wlsdpcks@naver.com>
2022-03-09 15:03:50 +0900

    useInput part Two

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
+  // validator는 유효성검사기능(사람들이 특수문자를 사용할 수 없게끔하는 기능)
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
+
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = (value) => value.length <= 10; // 입력한 글자의 길이가 10이 초과되지 않게끔 설정
+  // => !value.includes("@"); "@"가 입력되면 더이상 입력되지 않게끔 설정
+  const name = useInput("Mr.", maxLen);
   return (
     <div classname="App">
       <h1>Hello</h1>
